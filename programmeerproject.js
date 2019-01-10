75e44f77a944686dbf0948a4f7744e604b52d2cf
Rutger Storm <rutger.storm97@gmail.com>
2019-01-10 17:01:08 +0100

    Data in javascript

--- programmeerproject.js
+++ programmeerproject.js
@@ -3,5 +3,23 @@ Rutger Storm
 12444049
 */
 
+function loadingData(){
+d3.json("data_nation.json").then(function(data)
+{
+console.log(data)
+})
 
+d3.json("data_region1.json").then(function(data)
+{
+console.log(data)
+})
+d3.json("data_piechart.json").then(function(data)
+{
+console.log(data)
+})
 d3.json("data_agreement.json").then(function(data)
+{
+console.log(data)
+})
+}
+loadingData()
